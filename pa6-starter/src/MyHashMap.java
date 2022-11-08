8c36a159267cb3e41b5b220d8e7c1c61c9bf8a91
ayeUCSD <114363008+ayeUCSD@users.noreply.github.com>
2022-11-07 22:06:05 -0800

    did a few

--- pa6-starter/src/MyHashMap.java
+++ pa6-starter/src/MyHashMap.java
@@ -1,4 +1,6 @@
+import java.util.HashMap;
 import java.util.List;
+import java.util.LinkedList;
 import java.util.Objects;
 
 public class MyHashMap<K, V> implements DefaultMap<K, V> {
@@ -7,45 +9,69 @@ public class MyHashMap<K, V> implements DefaultMap<K, V> {
 	public static final String ILLEGAL_ARG_CAPACITY = "Initial Capacity must be non-negative";
 	public static final String ILLEGAL_ARG_LOAD_FACTOR = "Load Factor must be positive";
 	public static final String ILLEGAL_ARG_NULL_KEY = "Keys must be non-null";
-	
+
 	private double loadFactor;
 	private int capacity;
 	private int size;
 
 	// Use this instance variable for Separate Chaining conflict resolution
-	private List<HashMapEntry<K, V>>[] buckets;  
-	
-	// Use this instance variable for Linear Probing
-	private HashMapEntry<K, V>[] entries; 	
+	private List<HashMapEntry<K, V>>[] buckets;
+
+	// Use this instance variable for Linear Probing FUCK LINEAR PROBING ALL MY
+	// HOMIES HATE LINEAR PROBING
 
 	public MyHashMap() {
 		this(DEFAULT_INITIAL_CAPACITY, DEFAULT_LOAD_FACTOR);
+		this.capacity = DEFAULT_INITIAL_CAPACITY;
+		this.loadFactor = DEFAULT_LOAD_FACTOR;
 	}
-	
+
 	/**
 	 * 
 	 * @param initialCapacity the initial capacity of this MyHashMap
-	 * @param loadFactor the load factor for rehashing this MyHashMap
-	 * @throws IllegalArgumentException if initialCapacity is negative or loadFactor not
-	 * positive
+	 * @param loadFactor      the load factor for rehashing this MyHashMap
+	 * @throws IllegalArgumentException if initialCapacity is negative or loadFactor
+	 *                                  not positive
 	 */
 	@SuppressWarnings("unchecked")
 	public MyHashMap(int initialCapacity, double loadFactor) throws IllegalArgumentException {
-		// TODO Finish initializing instance fields
+		if (initialCapacity < 0) {
+			throw new IllegalArgumentException(ILLEGAL_ARG_CAPACITY);
+		} else {
+			this.capacity = initialCapacity;
+		}
 
+		if (loadFactor <= 0) {
+			throw new IllegalArgumentException(ILLEGAL_ARG_LOAD_FACTOR);
+		} else {
+			this.loadFactor = loadFactor;
+		}
 		// if you use Separate Chaining
 		buckets = (List<HashMapEntry<K, V>>[]) new List<?>[capacity];
-
-		// if you use Linear Probing
-		entries = (HashMapEntry<K, V>[]) new HashMapEntry<?, ?>[initialCapacity];
+		for(int i = 0; i < capacity; i++){
+			buckets[i] = new LinkedList<>();
+		}
 	}
 
 	@Override
 	public boolean put(K key, V value) throws IllegalArgumentException {
+
+		if (key == null) {
+			throw new IllegalArgumentException(ILLEGAL_ARG_NULL_KEY);
+		}
+
+		// check if we have space
+		if ((size + 1.0 / capacity) > loadFactor) {
+			expandRehash();
+		}
+
+		// prepwork: get index, make the entry
 		// can also use key.hashCode() assuming key is not null
-		int keyHash = Objects.hashCode(key); 
-		// TODO Auto-generated method stub
-		return false;
+		int keyHash = hashKey(key);
+		HashMapEntry<K, V> entry = new HashMapEntry<>(key, value);
+		buckets[keyHash].add(entry);
+		size++;
+		return true;
 	}
 
 	@Override
@@ -63,7 +89,7 @@ public class MyHashMap<K, V> implements DefaultMap<K, V> {
 	@Override
 	public void set(K key, V value) throws IllegalArgumentException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -95,12 +121,39 @@ public class MyHashMap<K, V> implements DefaultMap<K, V> {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+
+
+	private int hashKey(K key){
+		return key.hashCode() % capacity;
+	}
+
+	/**
+	 * this method should double the size of our list, and rehash all entries
+	 */
+	private void expandRehash() {
+		//double our capacity
+		capacity = capacity * 2;
+		List<HashMapEntry<K, V>>[] newBuckets = (List<HashMapEntry<K, V>>[]) new List<?>[capacity];
+		for(int i = 0; i < capacity; i++){
+			newBuckets[i] = new LinkedList<>();
+		}
+
+		for(List<HashMapEntry<K, V>> l : buckets){
+			for(HashMapEntry<K,V> entry : l){
+				int newHash = hashKey(entry.getKey());
+				newBuckets[newHash].add(entry);
+			}
+		}
+	}
+
 	
+
 	private static class HashMapEntry<K, V> implements DefaultMap.Entry<K, V> {
-		
+
 		K key;
 		V value;
-		
+
 		private HashMapEntry(K key, V value) {
 			this.key = key;
 			this.value = value;
@@ -115,7 +168,7 @@ public class MyHashMap<K, V> implements DefaultMap<K, V> {
 		public V getValue() {
 			return value;
 		}
-		
+
 		@Override
 		public void setValue(V value) {
 			this.value = value;
